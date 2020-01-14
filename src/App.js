5a0a50abc3fbbb570b1a077e144800173fcdef3a
Andrew Moeggenberg <andrew.moeggenberg.webdev@gmail.com>
2020-01-14 16:35:59 -0500

    api issue + css

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
   const getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},us&APPID=${API_KEY}`);
+    const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},us&APPID=${API_KEY}`);
     const data = await api_call.json();
     if (city) {
       setWeatherData(data);
--- src/components/Form.js
+++ src/components/Form.js
@@ -4,9 +4,9 @@ const Form = ({ getWeather, city, clearData }) => {
 
   return (
     <>
-      <form class="form" onSubmit={getWeather}>
-        <input class="form_input" type="text" name="city" placeholder="City" />
-        <button class="btn">Get Weather</button>
+      <form className="form" onSubmit={getWeather}>
+        <input className="form_input" type="text" name="city" placeholder="City" />
+        <button className="btn">Get Weather</button>
       </form>
       {city ? <button className="clear-btn" onClick={clearData}>Clear</button> :
         <button className="transparent">Clear</button>}
