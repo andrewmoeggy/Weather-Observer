fb754a869ec490729de5c6c390c8912079ce442c
Andrew Moeggenberg <andrew.moeggenberg.webdev@gmail.com>
2020-01-14 13:26:47 -0500

    css updates

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import { WiDaySunny } from 'react-icons/wi'
 
 const Title = () => {
   return (
     <>
+      <WiDaySunny className="main-icon" />
       <h1 className="title">Weather Observer</h1>
       <p className="title_paragraph">Enter in a U.S. city!</p>
     </>
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { WiGaleWarning, WiThermometer, WiCloud, WiHumidity, WiDirectionUp } from 'react-icons/wi';
 
 const Weather = ({ weatherData, city, tempurature, tempMin, tempMax, description, humidity, error }) => {
   console.log(weatherData)
@@ -9,12 +10,40 @@ const Weather = ({ weatherData, city, tempurature, tempMin, tempMax, description
 
   return (
     <div className="weather">
-      {city && <p>Location: {city}</p>}
-      {tempurature && <p>Tempurature: {formatTemp(tempurature)} &deg; F</p>}
-      {description && <p>Conditions: {description}</p>}
-      {humidity && <p>Humidity: {humidity} %</p>}
-      {tempMin && tempMax && <><p>Today's min: {formatTemp(tempMin)} &deg; F</p> <p>Today's max: {formatTemp(tempMax)} &deg; F</p></>}
-      {error && <p>{error}</p>}
+      {city &&
+        <p><WiGaleWarning className="weather_icon" />
+          Location:
+          <span className="weather_data">{city}</span>
+        </p>}
+      {tempurature &&
+        <p><WiThermometer className="weather_icon" />
+          Tempurature:
+          <span className="weather_data">{formatTemp(tempurature)} &deg; F</span>
+        </p>}
+      {description &&
+        <p><WiCloud className="weather_icon" />
+          Conditions:
+          <span className="weather_data">{description}</span>
+        </p>}
+      {humidity &&
+        <p><WiHumidity className="weather_icon" />
+          Humidity:
+          <span className="weather_data">{humidity} %</span>
+        </p>}
+      {tempMin && tempMax &&
+        <>
+          <p><WiDirectionUp className="weather_icon" />
+            Today's max:
+            <span className="weather_data">{formatTemp(tempMax)} &deg; F</span>
+          </p>
+          <p><WiDirectionUp className="weather_icon rotate" />
+            Today's min:
+            <span className="weather_data">{formatTemp(tempMin)} &deg; F</span>
+          </p>
+        </>
+      }
+      {error &&
+        <p>{error}</p>}
     </div>
   );
 }
