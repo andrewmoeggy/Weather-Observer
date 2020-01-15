a700fe3ae20d1685aa5855749139aec3e7973498
Andrew Moeggenberg <andrew.moeggenberg.webdev@gmail.com>
2020-01-15 17:44:34 -0500

    changed the name to weatherly

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,7 +5,7 @@ const Title = () => {
   return (
     <>
       <WiDaySunny className="main-icon" />
-      <h1 className="title">Weather Observer</h1>
+      <h1 className="title">Weatherly</h1>
       <p className="title_paragraph">Enter in a U.S. city!</p>
     </>
   );
